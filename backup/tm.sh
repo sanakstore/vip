802e16e30351ab69c833f942c1e58c84fe1b34da
sanakstore <144905341+sanakstore@users.noreply.github.com>
2023-11-19 04:26:51 +0800

    sanak store

--- backup/tm.sh
+++ backup/tm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt install figlet -y
-mkdir -p /etc/julak
-mkdir -p /etc/julak/theme
-cat <<EOF>> /etc/julak/theme/red
+mkdir -p /etc/sanak
+mkdir -p /etc/sanak/theme
+cat <<EOF>> /etc/sanak/theme/red
 BG : \E[40;1;41m
 TEXT : \033[0;31m
 EOF
@@ -11,23 +11,23 @@ cat <<EOF>> /etc/julak/theme/green
 BG : \E[40;1;42m
 TEXT : \033[0;32m
 EOF
-cat <<EOF>> /etc/julak/theme/yellow
+cat <<EOF>> /etc/sanak/theme/yellow
 BG : \E[40;1;43m
 TEXT : \033[0;33m
 EOF
-cat <<EOF>> /etc/julak/theme/blue
+cat <<EOF>> /etc/sanak/theme/blue
 BG : \E[40;1;44m
 TEXT : \033[0;34m
 EOF
-cat <<EOF>> /etc/julak/theme/magenta
+cat <<EOF>> /etc/sanak/theme/magenta
 BG : \E[40;1;95m
 TEXT : \033[0;95m
 EOF
-cat <<EOF>> /etc/julak/theme/cyan
+cat <<EOF>> /etc/sanak/theme/cyan
 BG : \E[40;1;46m
 TEXT : \033[0;36m
 EOF
-cat <<EOF>> /etc/julak/theme/color.conf
+cat <<EOF>> /etc/sanak/theme/color.conf
 magenta
 EOF
 clear
